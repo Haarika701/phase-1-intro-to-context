ec5239bc1731b798b4d309fa8f1af8b533cbe7e6
HaarikaR <hramadugu@gmail.com>
2022-01-13 11:55:19 -0800

    context...

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let createEmployeeRecord = function(row){
+    return{
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+
+}
+
+let createEmployeeRecords = function(employeeData){
+    return employeeData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(employee,dateStamp){
+    let[date,hour] = dateStamp.split(' ')
+    employee.timeInEvents.push({
+        type:"TimeIn",
+        hour: parseInt(hour,10),
+        date,
+    })
+   return employee
+}
